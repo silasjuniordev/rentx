07904d2fd89c53fcadebd06e85452ebf6c6c849c
Silas Junior <silasjuniordev@hotmail.com>
2024-03-26 23:52:08 -0300

    creating car card

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -10,8 +10,29 @@ import {
     HeaderContent,
     TotalCars
 } from "./styles";
+import { Car } from "../../components/Car";
 
 export function Home() {
+    const carDataOne = {
+        brand: 'Audi',
+        name: 'RS 5 Coupe',
+        rent: {
+            period: 'ao dia',
+            price: 120
+        },
+        thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png'
+    }
+
+    const carDataTwo = {
+        brand: 'Porsche',
+        name: 'Panamera',
+        rent: {
+            period: 'ao dia',
+            price: 340,
+        },
+        thumbnail: 'https://imgd-ct.aeplcdn.com/664x415/n/33vfbcb_1703039.jpg?q=80'
+    }
+
     return (
         <Container>
             <StatusBar 
@@ -30,6 +51,9 @@ export function Home() {
                     </TotalCars>
                 </HeaderContent>
             </Header>
+
+            <Car data={carDataOne} />
+            <Car data={carDataTwo} />
         </Container>
     )
 }
