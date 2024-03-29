603dfed6c99a8567e1ca875202e2e52d4da5993d
Silas Junior <silasjuniordev@hotmail.com>
2024-03-29 13:36:53 -0300

    Refactoring SplashScreen with fontsLoaded

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import * as SplashScreen from 'expo-splash-screen'
 import {
     useFonts,
     Inter_400Regular,
@@ -15,6 +16,8 @@ import { Scheduling } from './src/screens/Scheduling';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/styles/theme';
 
+SplashScreen.preventAutoHideAsync()
+
 export default function App() {
     const [fontsLoaded] = useFonts({
         Inter_400Regular,
@@ -24,14 +27,14 @@ export default function App() {
         Archivo_600SemiBold
     })
 
-    if(!fontsLoaded) {
-        return null
+    if(fontsLoaded) {
+        SplashScreen.hideAsync()
     }
 
     return (
         <ThemeProvider theme={theme}>
             <GestureHandlerRootView style={{ flex: 1 }}>
-                <Scheduling />
+                {fontsLoaded && <Scheduling />}
             </GestureHandlerRootView>
         </ThemeProvider>
     )
