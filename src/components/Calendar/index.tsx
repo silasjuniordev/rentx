bbd7beee590824e29103b8a833fabbfeb78295ac
Silas Junior <silasjuniordev@hotmail.com>
2024-03-29 12:05:52 -0300

    Creating Calendar component and adding in Scheduling page

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Calendar as CustomCalendar, LocaleConfig } from "react-native-calendars";
+import { Feather } from '@expo/vector-icons'
+import { useTheme } from "styled-components";
+
+LocaleConfig.locales['pt-br'] = {
+    monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+    dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+    dayNamesShort: ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SÁB'],
+    today: 'Hoje'
+}
+LocaleConfig.defaultLocale = 'pt-br'
+
+export function Calendar() {
+    const theme = useTheme()
+
+    return (
+        <CustomCalendar 
+            renderArrow={(direction) => 
+                <Feather 
+                    size={24}
+                    color={theme.colors.text}  
+                    name={direction === 'left' ? 'chevron-left' : 'chevron-right'}
+                />
+            }
+
+            headerStyle={{
+                backgroundColor: theme.colors.background_secondary,
+                borderBottomWidth: 0.5,
+                borderBottomColor: theme.colors.text_detail,
+                paddingBottom: 10,
+                marginBottom: 10
+            }}
+
+            theme={{
+                textDayFontFamily: theme.fonts.primary_400,
+                textDayHeaderFontFamily: theme.fonts.primary_500,
+                textDayHeaderFontSize: 10,
+                textMonthFontFamily: theme.fonts.secondary_600,
+                textMonthFontSize: 20,
+                monthTextColor: theme.colors.title,
+                arrowStyle: {
+                    marginHorizontal: -15
+                }
+            }}
+
+            firstDay={1}
+            minDate={new Date().toISOString()}
+        />
+    )
+}
--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -16,6 +16,7 @@ import {
 import { BackButton } from "../../components/BackButton";
 import { StatusBar } from "react-native";
 import { Button } from "../../components/Button";
+import { Calendar } from "../../components/Calendar";
 
 export function Scheduling() {
     const theme = useTheme()
@@ -59,7 +60,7 @@ export function Scheduling() {
             </Header>
 
             <Content>
-
+                <Calendar />
             </Content>
 
             <Footer>
